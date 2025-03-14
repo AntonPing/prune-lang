34df5f7e383f6dcfe19923f9d537d569c9297f15
Anton Ping <antonping1999@gmail.com>
2025-03-14 08:16:27 +0300

    Add more primitives

--- src/utils/prim.rs
+++ src/utils/prim.rs
@@ -1,6 +1,35 @@
 #[derive(Clone, Copy, Debug, Hash, Eq, PartialEq, PartialOrd, Ord)]
+pub enum Compare {
+    Lt,
+    Le,
+    Eq,
+    Ge,
+    Gt,
+    Ne,
+}
+
+#[derive(Clone, Copy, Debug, Hash, Eq, PartialEq, PartialOrd, Ord)]
 pub enum Prim {
+    /// integer arithmetics
     IAdd,
     ISub,
     IMul,
+    IDiv,
+    IRem,
+    INeg,
+
+    /// float-point arithmetics
+    // FAdd,
+    // FSub,
+    // FMul,
+    // FDiv,
+    // FNeg,
+
+    /// comparision
+    ICmp(Compare),
+
+    /// boolean operation
+    BAnd,
+    BOr,
+    BNot,
 }
