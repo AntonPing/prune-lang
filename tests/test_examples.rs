3a45d891dc32c15827e69ee4867813148e9d2463
Anton Ping <antonping1999@gmail.com>
2025-05-18 11:10:59 +0300

    Add new example `avl_tree`

--- tests/test_examples.rs
+++ tests/test_examples.rs
@@ -29,3 +29,13 @@ fn test_tree_insert_good() -> Result<(), ()> {
 fn test_tree_insert_bad() -> Result<(), ()> {
     action::test_bad_prog("tree_insert", "always_sorted", 5, 100, 5)
 }
+
+#[test]
+fn test_avl_tree_good() -> Result<(), ()> {
+    action::test_good_prog("avl_tree", "always_sorted_balanced", 5, 30, 5)
+}
+
+#[test]
+fn test_avl_tree_bad() -> Result<(), ()> {
+    action::test_bad_prog("avl_tree", "always_sorted_balanced", 5, 100, 5)
+}
