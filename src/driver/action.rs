ec45b7b5f778d5dfdab60ebca47d4ae44e73e13f
Anton Ping <antonping1999@gmail.com>
2025-07-25 09:04:05 +0300

    Refactor compiler pipeline and use new parser

--- src/driver/action.rs
+++ src/driver/action.rs
@@ -1,3 +1,4 @@
+use crate::driver::diagnostic::Diagnostic;
 use crate::logic::ast::*;
 use crate::syntax::{self, ast};
 use crate::utils::ident::Ident;
@@ -9,46 +10,53 @@ use std::{fs, io};
 
 use crate::tych::rename;
 
-pub enum PipeError {
-    FileNotExist(String),
-    ParseError(String),
-    RenameError(rename::RenameError),
-}
-
-pub struct Pipeline<'log, Log: io::Write> {
-    error: Vec<PipeError>,
+pub struct Pipeline<'src, 'log, Log: io::Write> {
+    src: &'src str,
     log: &'log mut Log,
+    diags: Vec<Diagnostic>,
 }
 
-impl<'log, Log: io::Write> Pipeline<'log, Log> {
-    pub fn new(log: &'log mut Log) -> Pipeline<'log, Log> {
+type PipeResult<T> = Result<T, ()>;
+
+impl<'src, 'log, Log: io::Write> Pipeline<'src, 'log, Log> {
+    pub fn new(src: &'src str, log: &'log mut Log) -> Pipeline<'src, 'log, Log> {
         Pipeline {
-            error: Vec::new(),
+            src,
             log,
+            diags: Vec::new(),
         }
     }
-    pub fn parse_program<S: AsRef<path::Path>>(&mut self, path: S) -> Result<ast::Program, ()> {
-        let src = fs::read_to_string(path).map_err(|err| {
-            let s = err.to_string();
-            self.error.push(PipeError::FileNotExist(s));
-            ()
-        })?;
-        let prog = syntax::parser_gen::parser::ProgramParser::new()
-            .parse(&src.as_str())
-            .map_err(|err| {
-                let s = err.to_string();
-                self.error.push(PipeError::ParseError(s));
-                ()
-            })?;
+
+    pub fn check_diag(&mut self) -> PipeResult<()> {
+        if self.diags.is_empty() {
+            return Ok(());
+        }
+        for diag in &self.diags {
+            write!(&mut self.log, "{}", diag.report(self.src, 10)).unwrap();
+        }
+        Err(())
+    }
+
+    // pub fn read_file<P: AsRef<path::Path>>(&mut self, path: P) -> PipeResult<String> {
+    //     let src = fs::read_to_string(path).map_err(|_err| {
+    //         self.diags.push(Diagnostic::error("file does not exist!"));
+    //     })?;
+    //     Ok(src)
+    // }
+
+    pub fn parse_program(&mut self) -> PipeResult<ast::Program> {
+        let prog = syntax::parser::parse_program(&mut self.diags, &self.src);
+        self.check_diag()?;
         Ok(prog)
     }
 
-    pub fn rename_pass(&mut self, prog: &mut ast::Program) -> Result<HashMap<Ident, Ident>, ()> {
+    pub fn rename_pass(&mut self, prog: &mut ast::Program) -> PipeResult<HashMap<Ident, Ident>> {
         let map = rename::rename_pass(prog).map_err(|errs| {
-            errs.into_iter().for_each(|err| {
-                self.error.push(PipeError::RenameError(err));
+            errs.into_iter().for_each(|_err| {
+                self.diags.push(Diagnostic::error("rename error!"));
             });
         })?;
+        self.check_diag()?;
         Ok(map)
     }
 
@@ -61,23 +69,23 @@ impl<'log, Log: io::Write> Pipeline<'log, Log> {
         let wlk = Walker::new(codes, self.log);
         Ok((wlk, entrys))
     }
-}
 
-pub fn test_prog<P: AsRef<path::Path>, Log: io::Write>(
-    path: P,
-    entry: &'static str,
-    start: usize,
-    end: usize,
-    step: usize,
-    log: &mut Log,
-) -> Result<bool, ()> {
-    let mut pipe = Pipeline::new(log);
-    let mut prog = pipe.parse_program(path)?;
-    let map = pipe.rename_pass(&mut prog)?;
-    let (mut wlk, entrys) = pipe.create_walker(&prog)?;
-    let entry = entrys[&PredIdent::Check(map[&Ident::dummy(&entry)])];
-    let res = wlk.run_loop(entry, start, end, step);
-    Ok(res)
+    pub fn test_prog(
+        &'log mut self,
+        entry: &'static str,
+        start: usize,
+        end: usize,
+        step: usize,
+    ) -> Result<bool, ()> {
+        let mut prog = self.parse_program()?;
+        let map = self.rename_pass(&mut prog)?;
+        let (mut wlk, entrys) = self.create_walker(&prog)?;
+        let entry = entrys
+            .get(&PredIdent::Check(map[&Ident::dummy(&entry)]))
+            .ok_or(())?;
+        let res = wlk.run_loop(*entry, start, end, step);
+        Ok(res)
+    }
 }
 
 pub fn test_unsat_prog<P: AsRef<path::Path>>(
@@ -92,8 +100,10 @@ pub fn test_unsat_prog<P: AsRef<path::Path>>(
     path.push("unsat");
     path.push(prog_name);
     path.set_extension("nrm");
+    let src = fs::read_to_string(path).map_err(|_err| ())?;
     let mut log = io::empty();
-    let res = test_prog(path, entry, start, end, step, &mut log)?;
+    let mut pipe = Pipeline::new(&src, &mut log);
+    let res = pipe.test_prog(entry, start, end, step)?;
     assert_eq!(res, false);
     Ok(())
 }
@@ -110,8 +120,10 @@ pub fn test_sat_prog<P: AsRef<path::Path>>(
     path.push("sat");
     path.push(prog_name);
     path.set_extension("nrm");
+    let src = fs::read_to_string(path).map_err(|_err| ())?;
     let mut log = io::empty();
-    let res = test_prog(path, entry, start, end, step, &mut log)?;
+    let mut pipe = Pipeline::new(&src, &mut log);
+    let res = pipe.test_prog(entry, start, end, step)?;
     assert_eq!(res, true);
     Ok(())
 }
